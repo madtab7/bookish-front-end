5e1c2b28c58f8b00dab135e063ba4be6a310de97
madtab7 <madtab7@gmail.com>
2018-11-02 15:26:02 -0400

    homepage and routing

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
 import SearchContainer from './SearchContainer';
 import UserContainer from './UserContainer';
+import { Container, Grid, Button } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
 export default class Home extends Component{
 
-  render(){
+  handleClick=(event)=>{
+    console.log(event.target.innerText)
+  }
 
+  render(){
+    //need to apply background image with id=home
     return(
-      <div>
-        hello
+      <div id="home">
+
+        <Grid columns={2} style={{marginTop:"10%"}}>
+          <Grid.Column style={{textAlign:"center"}}>
+            <Button size="massive" name="books" onClick={this.handleClick}>
+              <Link to="/books">
+                <h1 id="subhead">find a book</h1>
+              </Link>
+            </Button>
+          </Grid.Column>
+          <Grid.Column style={{textAlign:"center"}}>
+            <Button size="massive" name="profile" onClick={this.handleClick}>
+              <Link to="/profile">
+                <h1 id="subhead">see my bookshelf</h1>
+              </Link>
+            </Button>
+          </Grid.Column>
+        </Grid>
+
       </div>
     )
   }
 }
+
+// <h1 id="subhead" name="profile">see my bookshelf</h1>

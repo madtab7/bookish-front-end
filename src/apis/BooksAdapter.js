469821e4cd0d6841408e13ab295fd48b7aa3a90e
madtab7 <madtab7@gmail.com>
2019-02-19 15:41:08 -0500

    working nyt api & amazon external link

--- src/apis/BooksAdapter.js
+++ src/apis/BooksAdapter.js
@@ -1,5 +1,6 @@
 const google_api_key = process.env.REACT_APP_GOOGLE_BOOKS_API_KEY
-const nyt_api_key = process.env.REACT_APP_NYT_BOOKS_API_KEY
+// const nyt_api_key = process.env.REACT_APP_NYT_BOOKS_API_KEY
+const nyt_api_key = "zqJGLbF1G35GWzGXmsuAoOwRYGgpjkGa"
 
 const GOOGLE_Q_ENDPOINT = 'https://www.googleapis.com/books/v1/volumes?q='
 const NYT_LIST_ENDPOINT = 'https://api.nytimes.com/svc/books/v3/lists/'
@@ -36,7 +37,8 @@ export default class BooksAdapter {
 
   //handle amazon link for purchase
   static getAmazonLink(isbn){
-    return (`https://www.amazon.com/gp/search/ref=sr_adv_b/?search-alias=stripbooks&unfiltered=1&field-keywords=&field-author=&field-title=&field-isbn=${isbn}&field-publisher=&node=&field-p_n_condition-type=&p_n_feature_browse-bin=&field-age_range=&field-language=&field-dateop=During&field-datemod=&field-dateyear=&sort=relevanceexprank&Adv-Srch-Books-Submit.x=31&Adv-Srch-Books-Submit.y=10`);
+    // return (`https://www.amazon.com/gp/search/ref=sr_adv_b/?search-alias=stripbooks&unfiltered=1&field-keywords=&field-author=&field-title=&field-isbn=${isbn}&field-publisher=&node=&field-p_n_condition-type=&p_n_feature_browse-bin=&field-age_range=&field-language=&field-dateop=During&field-datemod=&field-dateyear=&sort=relevanceexprank&Adv-Srch-Books-Submit.x=31&Adv-Srch-Books-Submit.y=10`);
+    return(`https://www.amazon.com/s?k=${isbn}&i=stripbooks&ref=nb_sb_noss`)
 
   }
 
